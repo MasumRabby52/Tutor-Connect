c96409684f2a9f42e8a8b030a3998f71e0b56ab3
MasumRabby52 <junaedmasumrabby@gmail.com>
2025-07-29 20:52:26 +0600

    new commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [
     react(),
     mode === 'development' &&
-    componentTagger(),
+      componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
